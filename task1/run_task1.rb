5fc0de3996e5b4c92994ffcf35e5f30a20ea7be6
HSCOBen <hsco.ben@gmail.com>
2020-08-14 11:35:45 -0400

    hfhgfhgfh

--- task1/run_task1.rb
+++ task1/run_task1.rb
@@ -1,7 +1,4 @@
 require "mini_magick"
 
-image = MiniMagick::Image.open("task1/input/image.tif")
-puts image.width
-
-
-puts 'DONE'
+image = MiniMagick::Image.open("task1/input/5f217ff5effd2.jpg")
+image.get_pixels
