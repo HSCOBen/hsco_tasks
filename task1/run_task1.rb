f814d3bc5a77d3ae0638ebdcf39da46e70625d7c
Charlie Prezzano <charlie@hawthornethreads.com>
2020-08-17 09:19:21 -0400

    Fixed path to image ... iterate through pixels of the image, display the pixels RGB value

--- task1/run_task1.rb
+++ task1/run_task1.rb
@@ -1,4 +1,12 @@
 require "mini_magick"
 
-image = MiniMagick::Image.open("task1/input/5f217ff5effd2.jpg")
-image.get_pixels
+INPUT_IMAGE_PATH = "input/5f217ff5effd2.jpg"
+
+image = MiniMagick::Image.open(INPUT_IMAGE_PATH)
+image_pixels = image.get_pixels
+
+(1..image.width).each { |x|
+  (1..image.height).each { |y|
+    puts "At pixel (" + x.to_s + "," + y.to_s + "), RGB Color: " + image_pixels[x][y].to_s
+  }
+}
